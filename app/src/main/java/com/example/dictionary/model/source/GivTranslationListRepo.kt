98478a9ece981044803f9c12c1611c7fd6eac0c2
Dmitrii <dim1928@yandex.ru>
2023-05-09 03:12:19 +0300

    Подлючил реактивщину к проекту, но не релизовал. Планирую реализвоать в MVVM

--- app/src/main/java/com/example/dictionary/model/source/GivTranslationListRepo.kt
+++ app/src/main/java/com/example/dictionary/model/source/GivTranslationListRepo.kt
@@ -2,6 +2,7 @@ package com.example.dictionary.model.source
 
 import com.example.dictionary.model.data.TranslationListEntityItem
 
+
 interface GivTranslationListRepo {
     fun getTranslationList(
         query: String,
